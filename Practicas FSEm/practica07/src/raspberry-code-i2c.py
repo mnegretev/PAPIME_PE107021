325bf3bbbd99d6dffeab9318bf9e10031431f1fa
Mauricio Matamoros <kyordhel@gmail.com>
2021-09-25 18:35:50 -0500

    Add practikum 07

---
+++ Practicas FSEm/practica07/src/raspberry-code-i2c.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ## #############################################################
+#
+# Author: Mauricio Matamoros
+# Date:
+#
+# ## ############################################################
+# Future imports (Python 2.7 compatibility)
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import smbus
+import struct
+import time
+
+# Arduino's I2C device address
+SLAVE_ADDR = 0x0A # I2C Address of Arduino 1
+
+# Initialize the I2C bus;
+# RPI version 1 requires smbus.SMBus(0)
+i2c = smbus.SMBus(1)
+
+def readPower():
+	try:
+		# Creates a message object to read 4 bytes from SLAVE_ADDR
+		msg = smbus2.i2c_msg.read(SLAVE_ADDR, 4)
+		i2c.i2c_rdwr(msg)  # Performs write
+		data = list(msg)   # Converts stream to list
+		pwr = struct.unpack('<f', ''.join([chr(c) for c in data]))
+		# print('Received temp: {} = {}'.format(data, pwr))
+		return pwr
+	except:
+		return None
+
+def writePower(pwr):
+	try:
+		data = struct.pack('<f', pwr) # Packs number as float
+		# Creates a message object to write 4 bytes from SLAVE_ADDR
+		msg = smbus2.i2c_msg.write(SLAVE_ADDR, data)
+		i2c.i2c_rdwr(msg)  # Performs write
+	except:
+		pass
+
+def main():
+	while True:
+		try:
+			power = input("Power? ")
+			power = float(power)
+			if power >= 0 and power <= 100:
+				writePower(power)
+				print("\tPower set to {}".format(readPower()))
+			else:
+				print("\tInvalid!")
+		except:
+			print("\tInvalid!")
+
+if __name__ == '__main__':
+	main()
